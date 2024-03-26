da740c984a043c02895a8c33cc88d1b911b9db13
AyimNelson <ayimnelson01@gmail.com>
2024-03-26 18:46:29 +0000

    Built TaskList, added urls path for TaskList

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    template_name = "todo/task_list.html"
+    context_object_name = "tasks"
